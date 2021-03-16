cf26e51bcfe0f66d493ea6c17003080637529ec2
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-17 03:10:30 +0500

    Update Dashboard to activate input form and display urls list
    1:- Updated
    	a:- src/components/dashBoard/logedin/
    		i:- AppLogedIn.tsx to activate input form and display bookmarks list
    		ii:- appLogedIn.css to add styling bookmarks list

--- src/components/dashBoard/logedIn/AppLogedIn.tsx
+++ src/components/dashBoard/logedIn/AppLogedIn.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { AppHead } from "../addOns/AppHead";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { TextField } from "@material-ui/core";
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import "./appLogedIn.css";
+import { Card, CardContent } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import EditIcon from "@material-ui/icons/Edit";
+import { makeStyles } from "@material-ui/core/styles";
 
 // Type Defination
 interface BookmarkProps {
@@ -19,7 +23,36 @@ const initialValues: BookmarkProps = {
   url: "",
 };
 
+const bookmarksReducer = (state, action) => {
+  switch (action.type) {
+    case "addBookmark":
+      return [
+        {
+          id: Math.floor(Math.random() * 100000000000000),
+          desc: action.payload.desc,
+          url: action.payload.url,
+        },
+        ...state,
+      ];
+  }
+};
+
+const useStyles = makeStyles((theme) => ({
+  viewButton: {
+    background: "rgb(255, 60, 0)",
+    "&:hover": {
+      backgroundColor: "rgba(0, 128, 0, 0.733)",
+    },
+    textDecoration: "none",
+    color: "white",
+  },
+}));
+
 export const AppLogedIn = () => {
+  //  useStyles
+  const classes = useStyles();
+  const [bookmarks, dispatch] = useReducer(bookmarksReducer, []);
+  console.log("from reducer", bookmarks);
   return (
     <div>
       <AppHead />
@@ -32,7 +65,7 @@ export const AppLogedIn = () => {
           validationSchema={Yup.object({
             desc: Yup.string()
               .min(3, "Must be 3 characters or more")
-              .max(15, "Must be 25 characters or less")
+              .max(25, "Must be 25 characters or less")
               .required("Kindly add Description"),
             url: Yup.string()
               .matches(
@@ -42,7 +75,7 @@ export const AppLogedIn = () => {
               .required("Please enter website"),
           })}
           onSubmit={(values, onSubmitProps) => {
-            console.log(values);
+            dispatch({ type: "addBookmark", payload: values });
             onSubmitProps.resetForm();
           }}
         >
@@ -90,6 +123,70 @@ export const AppLogedIn = () => {
           </Form>
         </Formik>
       </div>
+      {/* ****** */}
+      {/* ****** */}
+      {/* Display Portion */}
+      {/* ****** */}
+      {/* ****** */}
+      {bookmarks.length === 0 ? (
+        <div className='taskScreen taskScreenE'>
+          <p>No Saved Bookmarks</p>
+        </div>
+      ) : (
+        <div className='bookmarkScreen'>
+          <div className='bookMarksContainer'>
+            <Grid container className='bookmarkGrid'>
+              {bookmarks.map((bookmark, i) => (
+                <Grid
+                  key={i}
+                  item
+                  xs={8}
+                  md={2}
+                  component={Card}
+                  className='bookmarkCard'
+                >
+                  <CardContent>
+                    <h3 className='bookmarkName'>
+                      <strong>{bookmark.desc}</strong>
+                    </h3>
+                    <div className='viewDiv'>
+                      <Button
+                        variant='contained'
+                        focusRipple
+                        className={classes.viewButton}
+                        aria-label='visit'
+                        onClick={() => {
+                          alert("view");
+                        }}
+                      >
+                        Visit
+                      </Button>
+                    </div>
+                    <div className='editdeleteButtons'>
+                      <Button
+                        aria-label='delete'
+                        onClick={() => {
+                          alert("delete");
+                        }}
+                      >
+                        <DeleteForeverIcon style={{ color: "red" }} />
+                      </Button>
+                      <Button
+                        aria-label='Edit'
+                        onClick={() => {
+                          alert("edit");
+                        }}
+                      >
+                        <EditIcon style={{ color: "blue" }} />
+                      </Button>
+                    </div>
+                  </CardContent>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        </div>
+      )}
       {/* ****** */}
       {/* ****** */}
     </div>
